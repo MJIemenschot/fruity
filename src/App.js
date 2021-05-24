e13207d31e72c8784b5839e66f384a5cf664d7f2
MJSIemenschot <m.iemenschot@outlook.com>
2021-05-24 17:59:35 +0200

    changes 3

--- src/App.js
+++ src/App.js
@@ -7,49 +7,95 @@ import { ReactComponent as ShoppingCart } from './assets/winkelmandje.svg';
 import './App.css';
 
 function App() {
-  return (
-      <fragment>
-          <nav>
-              <ul>
-                  <li><a href ="/">Shop</a></li>
-                  <li><a href ="/">Ons verhaal</a></li>
-                  <li><a href ="/">Blog</a></li>
-              </ul>
-              <ShoppingCart className="shopping-cart-icon" />
-          </nav>
-          <header>
-              <h1>Fruit perfection</h1>
-              <button
-                  type="button"
-                onClick={() => console.log("Jij wil shoppen!")}
-              >
-                  Shop nu
-              </button>
-          </header>
-      <main>
-          <Product
-              image={citroenen}
-              title="Citroen"
-              description=" citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken."
-          />
-          <Product
+    const [messageValue, setMessageValue] = React.useState('');
+    const [checkedTerms, toggleCheckedTerms] = React.useState(false);
+    const [submitted, setSubmitted] =React.useState(false);
 
-              image={limoenen}
-              title="Limoen"
-              description="Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen."
+    function sendForm(){
+        console.log(`Het bericht: "${messageValue}" is succesvol verzonden.`);
+    }
 
-          />
-          <Product
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Shop</a>
+                    </li>
+                    <li>
+                        <a href="/">Ons verhaal</a>
+                    </li>
+                    <li>
+                        <a href="/">Blog</a>
+                    </li>
+                </ul>
+                <ShoppingCart className="shopping-cart-icon" />
+            </nav>
+            <header>
+                <h1>Fruit perfection</h1>
+                <button
+                    type="button"
+                    onClick={() => console.log("Jij wil shoppen!")}
+                >
+                    Shop nu
+                </button>
+            </header>
+            <main>
+                <Product
+                    image={citroenen}
+                    title="Citroen"
+                    description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+                />
+                <Product
+                    image={limoenen}
+                    title="Limoen"
+                    description="Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen."
+                />
+                <Product
+                    image={ijsblokjes}
+                    title="IJsblokjes"
+                    description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje. Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
+                />
+            </main>
+            <footer>
+                <div className="form-container">
+                    <h2>Contactformulier</h2>
 
-              image={ijsblokjes}
-              title="Ijsblokjes"
-              description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje. Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Typ hier jouw bericht"
+                            name="message"
+                            className={messageValue.length > 20 ? 'input-error' : ''}
+                            value={messageValue}
+                            onChange={(e) => setMessageValue(e.target.value)}
+                        />
+                        {messageValue.length > 20 && <p className="error-message">Dit bericht is te lang!</p>}
 
-          />
+                        <label htmlFor="terms-and-conditions">
+                            <input
+                                type="checkbox"
+                                name="terms-and-conditions"
+                                id="terms-and-conditions"
+                                checked={checkedTerms}
+                                onChange={() => toggleCheckedTerms(!checkedTerms)}
+                            />
 
-    </main>
-      </fragment>
-  );
+                            Ik ga akkoord met de algemene voorwaarden
+                        </label>
+
+                        <button
+                            type="submit"
+                            disabled={!checkedTerms}
+                            onClick={sendForm}
+                        >
+                            Verstuur
+                        </button>
+                    </form>
+                </div>
+            </footer>
+        </>
+    );
 }
 
 export default App;
